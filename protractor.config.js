a13e6a9f4c1ddbc969949b3086eb20bfae36f47c
Salomon Orrego Martinez <sorregom@pslcol.com.co>
2017-09-28 16:02:03 -0500

    orden de codigo protractor.config.js

--- protractor.config.js
+++ protractor.config.js
@@ -1,8 +1,8 @@
 exports.config = {
-    framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['test/Google.spec.js'],
-    onPrepare: () => {
-      browser.ignoreSynchronization = true;
-    }
-   }
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['test/Google.spec.js'],
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+  }
+}
