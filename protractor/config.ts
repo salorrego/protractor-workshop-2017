e2103e8bba195377ee7812c3eb989d77ea90394c
Salomon Orrego Martinez <sorregom@pslcol.com.co>
2017-09-29 08:11:20 -0500

    configurando typescript

---
+++ protractor/config.ts
@@ -0,0 +1,11 @@
+import { browser, Config } from 'protractor';
+
+export let config: Config = {
+ framework: 'jasmine',
+ seleniumAddress: 'http://localhost:4444/wd/hub',
+ specs: ['../test/Google.spec.js'],
+ noGlobals: true,
+ onPrepare: () => {
+   browser.ignoreSynchronization = true;
+ }
+}
