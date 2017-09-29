36016ea02e32848b7df91b0e1384326e3a4adf9f
salorrego <salorrego@gmail.com>
2017-09-29 14:37:34 -0500

    Chrome Headless

---
+++ protractor/headless.conf.ts
@@ -0,0 +1,19 @@
+import { browser, Config } from 'protractor';
+import { reporter }   from './helpers/reporter';
+
+export let config: Config = {
+ framework: 'jasmine',
+ SELENIUM_PROMISE_MANAGER: false,
+ specs: ['../test/Google.spec.js'],
+ noGlobals: true,
+ onPrepare: () => {
+   browser.ignoreSynchronization = true;
+   reporter();
+ },
+ capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+    }
+  } 
+}
