477efedf80f336e44627c95a0482261fec6352a5
Salomon Orrego Martinez <sorregom@pslcol.com.co>
2017-10-02 11:46:20 -0500

    shipping step pom

---
+++ src/page/ShippingStep.page.ts
@@ -0,0 +1,19 @@
+import { $, ElementFinder, promise } from 'protractor';
+
+export class ShippingStepPage {
+  private get checkButtonTermsOfService(): ElementFinder {
+      return $('#cgv');
+  }
+
+  private get proceedToCheckoutButton(): ElementFinder {
+    return $('#form > p > button > span');
+  }
+    
+  public agreeTermsOfService(): promise.Promise<void> {
+      return this.checkButtonTermsOfService.click();
+  }
+    
+  public proceedToCheckout(): promise.Promise<void> {
+    return this.proceedToCheckoutButton.click();
+  }
+}
