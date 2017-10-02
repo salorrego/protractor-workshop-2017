ede5cd128bcd3bf1d749f62a7562ae20d0e78245
Salomon Orrego Martinez <sorregom@pslcol.com.co>
2017-10-02 09:42:13 -0500

    ProductList page

---
+++ src/page/ProductList.page.ts
@@ -0,0 +1,11 @@
+import { $, ElementFinder, promise } from 'protractor';
+
+export class ProductListPage {
+    private get procceedToCheckoutButton(): ElementFinder {
+        return $('.button-container > a');
+      }
+     
+      public productListCheckout(): promise.Promise<void> {
+        return this.procceedToCheckoutButton.click();
+      }
+}
