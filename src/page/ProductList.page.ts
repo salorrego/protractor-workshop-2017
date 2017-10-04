dbbf4d49c01f24e2a890b7569bb466710399cea5
sorregom <sorregom@psl.com.co>
2017-10-04 09:42:16 -0500

    correccion de espera hasta que el elemento sea visible

--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -1,11 +1,14 @@
-import { $, ElementFinder, promise } from 'protractor';
+import { $,browser, ElementFinder, ExpectedConditions } from 'protractor';
 
 export class ProductListPage {
+  private until = ExpectedConditions;
+
   private get procceedToCheckoutButton(): ElementFinder {
     return $('.button-container > a');
   }
     
-  public productListCheckout(): promise.Promise<void> {
+  public async productListCheckout(): Promise<void> {
+    await browser.wait(this.until.visibilityOf(this.procceedToCheckoutButton));
     return this.procceedToCheckoutButton.click();
   }
 }
