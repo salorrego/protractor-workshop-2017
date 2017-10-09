b4cfb85c7ae5721510bb9c878c365520b2b4c341
Salomon Orrego Martinez <sorregom@psl.com.co>
2017-10-09 16:06:08 -0500

    locators test 1

--- src/page/ProductDetail.page.ts
+++ src/page/ProductDetail.page.ts
@@ -2,7 +2,7 @@ import { $, ElementFinder, promise } from 'protractor';
 
 export class ProductDetailPage {
   private get productDetail(): ElementFinder {
-    return $('#center_column > ul > li > div > div.left-block > div > a.product_img_link > img');
+    return $('[title="Faded Short Sleeve T-shirts"] > img');
   }
     
   public goToProductDetail(): promise.Promise<void> {
