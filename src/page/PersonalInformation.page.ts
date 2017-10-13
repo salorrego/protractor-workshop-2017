2819c59fa74e3a9a5c72d3d2c41eb8fe1542615d
Salomon Orrego Martinez <salorrego@gmail.com>
2017-10-13 07:23:46 -0500

    cambio de '$' por 'by...'

--- src/page/PersonalInformation.page.ts
+++ src/page/PersonalInformation.page.ts
@@ -1,51 +1,52 @@
-import { $, ElementFinder, promise, by, browser, ExpectedConditions } from 'protractor';
+import { browser, ElementFinder, promise, by, element, ExpectedConditions } from 'protractor';
 
 export class PersonalInformationPage {
   private get buttonSubmit(): ElementFinder {
-    return $('#submit');
+    return element(by.id('submit'));
   }
 
   private get firstNameField(): ElementFinder {
-    return $('input[name="firstname"]');
+    return element(by.css('input[name="firstname"]'));
   }
 
   private get lastNameField(): ElementFinder {
-    return $('input[name="lastname"]');
+    return element(by.css('input[name="lastname"]'));
   }
 
   private get title(): ElementFinder {
-    return $('#content > h1');
+    return element(by.id('content')).element(by.tagName('h1'));
   }
   
   private chooseProfession(profession): void {
     profession.forEach(async (element) => {
-      await $(`input[value="${element}"]`).click();
+      await element(by.css(`input[value="${element}"]`)).click();
     });
   }
   
   private chooseTools(tools): void {
     tools.forEach(async (element) => {
-      await $(`input[value="${element}"]`).click();
+      await element(by.css(`input[value="${element}"]`)).click();
     });
   }
 
   private clickCommands(commands): void {
-    const commandsSelect = $('#selenium_commands');
+    const commandsSelect = element(by.id('selenium_commands'));
     commands.forEach(async (element) => {
       await commandsSelect.element(by.cssContainingText('option', element)).click();
     });
   }
 
   private continentChoose(continent): ElementFinder {
-    return $(`#continents`).element(by.cssContainingText('option', continent));
+    return element(by.id('continents'))
+      .element(by.cssContainingText('option', continent));
   }
 
   private experience(exp): ElementFinder {
-    return $(`#exp-${exp - 1}`);
+    return element(by.id(`exp-${exp - 1}`));
   }
 
   private sexField(sex): ElementFinder {
-    return $(`input[value="${sex}"]`);
+    return element(by.css(`input[value="${sex}"]`));
   }
   
   public async getTitle(): Promise<string> {
