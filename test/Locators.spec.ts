cad9a205dd7531e5134fb132d1570fcc4a358117
Salomon Orrego Martinez <salorrego@gmail.com>
2017-10-13 10:19:36 -0500

    testing failure

--- test/Locators.spec.ts
+++ test/Locators.spec.ts
@@ -1,7 +1,7 @@
 import { browser } from 'protractor';
 import { PersonalInformationPage } from '../src/page';
 
-describe('Given the page to form fill', () => {
+fdescribe('Given the page to form fill', () => {
   const urlBase = 'http://toolsqa.com/automation-practice-form/';
 
   beforeAll(async () => {
